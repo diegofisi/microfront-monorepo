fa6fd82557ec4160863cea24407deca423d46d15
DiegoGVH <diego704249@gmail.com>
2025-06-01 21:25:17 -0500

    Initial commit

---
+++ apps/first/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@microfront-monorepo/first" />
+    </div>
+  );
+}
+
+export default App;
